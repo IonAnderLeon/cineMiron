525dc374dacbf9094a33071b5c49291d349fb9c8
David <smdavid1999@gmail.com>
2025-11-27 17:18:48 +0000

    Reestructuración

--- src/main/java/com/example/cinemiron/ui/theme/Color.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cinemiron.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val PrimaryDark = Color(0xFF304347)
-val Primary = Color(0xFF506F6A)
-val PrimaryLight = Color(0xFF7B9A92)
-val Accent = Color(0xFFC4D1BF)
-val AccentLight = Color(0xFFDEDAB5)
